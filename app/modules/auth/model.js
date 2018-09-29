4f412ef313b062b552cccfecd638a448bc0be59c
Faiz <891366402@qq.com>
2018-09-29 19:50:39 +0800

    login结束

---
+++ app/modules/auth/model.js
@@ -0,0 +1,7 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+    token: DS.attr('string'),
+    token_expire: DS.attr('number'),
+    user: DS.belongsTo('user', { async: true })
+});
--- app/modules/index/controller.js
+++ app/modules/index/controller.js
@@ -1,9 +1,11 @@
 import Controller from '@ember/controller';
 import { computed } from '@ember/object';
 import { getOwner } from '@ember/application';
+import { inject } from '@ember/service';
 import loginModel from './login';
 
 export default Controller.extend({
+    cookies: inject(),
     email: null,
     password: null,
     login: computed(function () {
@@ -13,9 +15,25 @@ export default Controller.extend({
     }),
     actions: {
         submit() {
-            this.transitionToRoute('project-sort')
-            // console.info(this.login.email)
-            // console.info(this.login.password)
+            let req = this.store.createRecord('request', { res: 'user' });
+
+            let eqValues = [
+                { type: 'eqcond', key: 'email', val: this.login.email },
+                { type: 'eqcond', key: 'password', val: this.login.password },
+            ]
+
+            eqValues.forEach((elem, index) => {
+                req.get(elem.type).pushObject(this.store.createRecord(elem.type, {
+                    key: elem.key,
+                    val: elem.val,
+                }))
+            });
+            let conditions = this.store.object2JsonApi('request', req);
+            
+            this.store.queryObject('/api/v1/login/0', 'auth', conditions).then(data => {
+                this.get('cookies').write('token', data.token, { path: '/', maxAge: data.token_expire});
+                this.transitionToRoute('project-sort')
+            })
         }
     }
 });
