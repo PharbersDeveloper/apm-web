a2a63336a744915f52cfafc84781c51e0600ae96
Faiz <891366402@qq.com>
2018-10-16 11:18:55 +0800

    删除demo 路由

--- app/router.js
+++ app/router.js
@@ -15,23 +15,22 @@ Ember.Route.reopen({
 })
 
 Router.map(function () {
-    this.route('project-sort');
-    this.route('new-project', function () {
-        this.route('project-start', { path: '/project-start/:courseid/:paperid'}, function () {
-            this.route('index', { path: '/' }, function () {
-                this.route('analyze');
-                this.route('resource');
-                this.route('action-plan');
-                this.route('upshot');
-                this.route('sort');
-                this.route('objective');
-            });
-        });
-    });
-    this.route('history-project');
-    this.route('perfect-info');
-    this.route('sign_up');
-    this.route('demo');
+  this.route('project-sort');
+  this.route('new-project', function () {
+      this.route('project-start', { path: '/project-start/:courseid/:paperid'}, function () {
+          this.route('index', { path: '/' }, function () {
+              this.route('analyze');
+              this.route('resource');
+              this.route('action-plan');
+              this.route('upshot');
+              this.route('sort');
+              this.route('objective');
+          });
+      });
+  });
+  this.route('history-project');
+  this.route('perfect-info');
+  this.route('sign_up');
 });
 
 export default Router;
