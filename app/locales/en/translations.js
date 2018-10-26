cbb9724ccc1d03e5e16ea19b2fff020a0b802762
Chenghao Sun <601330984@qq.com>
2018-10-26 20:37:32 +0800

    除了核心业务的页面与组件 其他部分已改为i18n

--- app/locales/en/translations.js
+++ app/locales/en/translations.js
@@ -8,9 +8,83 @@ export default {
   // },
   //
   // "key.with.interpolation": "Text with {{anInterpolation}}"
-  "common": {
-    "": {
-      "": ""
-    }
+  
+  "apm": {
+    "sysName": "ＳＹＳ ＮＡＭＥ",
+    "account": "Account",
+    "password": "Password",
+    "signIn": "Sign in",
+    "mail": "Mail",
+    "verificationCode": "Verification code",
+    "send": "Send",
+    "confirm": "Confirm",
+    "cancel": "Cancel",
+    "close": "Close",
+    "click": "Click",
+    "determine": "Determine",
+    "signUp": "Sign up",
+    "logOut": "Log out",
+
+    "index": {
+      "slogan": "slogan",
+      "forgetPassword": "Forget password?",
+      "doNotHaveAccount": "Do not have account?",
+      "clickToSignUp": "Click to sign up",
+    },
+
+    "sign": {
+      "passwordHint": "8-20 combination of letters and numbers",
+      "userSignUp": "User sign up",
+      "sendVerificationCode": "Send verification code",
+      "mail": "mail",
+      "mailVerificationCode": "Mail verification code",
+      "loginPassword": "Login password",
+      "confirmPassword": "Confirm password",
+      "confirmSignUp": "Confirm sign up",
+      "hadAccount": "Already have account?",
+      "clickLogin": "Click to login",
+      "completeBasicInfo": "Complete basic information",
+      "name": "Name",
+      "mobile": "Mobile number",
+      "mobileVerificationCode": "Mobile verification code",
+      "corpName": "Corporate name",
+      "position": "Position",
+      "signUpSuccess": "Sign up success",
+      "goToLogin": "Go to login",
+    },
+
+    "projectSort": {
+      "slogan": "sloganslogansloganslogansloganslogan",
+      "newProject": "Start new project",
+      "hisProject": "Historical project",
+    },
+
+    "newProject": {
+      "title": "New project",
+    },
+
+    "historyProject": {
+      "hisPlan": "Historical plan",
+      "ongoing": "Ongoing",
+      "done": "Done",
+    },
+
+    "component": {
+      "classCard":{
+        "soon": "Coming soon",
+        "start": "Start",
+        "skip": "Skip",
+        "enterSceneInfo": "Entering scene information",
+      },
+
+      "hisClassCard":{
+        "title": "Regional planning training course",
+        "date": "Date:",
+        "continue": "Continue",
+        "examine": "Examine",
+      },
+
+    },
+
   },
 };
--- app/locales/zh/translations.js
+++ app/locales/zh/translations.js
@@ -8,9 +8,83 @@ export default {
   // },
   //
   // "key.with.interpolation": "Text with {{anInterpolation}}"
-  "common": {
-    "": {
-      "": ""
-    }
+
+  "apm": {
+    "sysName": "ＳＹＳ ＮＡＭＥ",
+    "account": "帐号",
+    "password": "密码",
+    "signIn": "登入",
+    "mail": "邮箱",
+    "verificationCode": "验证码",
+    "send": "发送",
+    "confirm": "确认",
+    "cancel": "取消",
+    "close": "关闭",
+    "click": "点击",
+    "determine": "确定",
+    "signUp": "注册",
+    "logOut": "退出系统",
+    
+    "index": {
+      "slogan": "slogan",
+      "forgetPassword": "忘记密码?",
+      "doNotHaveAccount": "还没有帐号?",
+      "clickToSignUp": "点击注册",
+    },
+
+    "sign": {
+      "userSignUp": "用户注册",
+      "sendVerificationCode": "发送验证码",
+      "mail": "邮箱",
+      "mailVerificationCode": "邮箱验证码",
+      "passwordHint": "8-20位字母和数字组合",
+      "loginPassword": "登入密码",
+      "confirmPassword": "确认密码",
+      "confirmSignUp": "确定注册",
+      "hadAccount": "已有帐号？",
+      "clickLogin": "点击登录",
+      "completeBasicInfo": "完善基础信息",
+      "name": "姓名",
+      "mobile": "手机",
+      "mobileVerificationCode": "手机验证码",
+      "corpName": "公司名称",
+      "position": "职位",
+      "signUpSuccess": "注册成功",
+      "goToLogin": "前往登入",
+    },
+
+    "projectSort": {
+      "slogan": "sloganslogansloganslogansloganslogan",
+      "newProject": "Start new project",
+      "hisProject": "Historical project",
+    },
+
+    "newProject": {
+      "title": "New project",
+    },
+
+    "hisProject": {
+      "hisPlan": "Historical plan",
+      "ongoing": "进行中",
+      "done": "已完成",
+    },
+
+    "component": {
+      "classCard":{
+        "soon": "即将推出",
+        "start": "Start",
+        "skip": "跳过",
+        "enterSceneInfo": "进入场景信息",
+      },
+
+      "hisClassCard":{
+        "title": "区域计划培训课",
+        "date": "日期：",
+        "continue": "继续",
+        "examine": "查看",
+      },
+
+    },
+
   },
 };
